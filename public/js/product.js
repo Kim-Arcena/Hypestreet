8d482d1156d61118304ec7137b4c981c96256fa3
Kimberly Arcena <kvarcena@up.edu.ph>
2022-07-14 18:34:08 +0800

    slider animation

--- public/js/product.js
+++ public/js/product.js
@@ -1,3 +1,18 @@
+const productImages = document.querySelectorAll(".product-images img");
+const productImageSlide = document.querySelector(".image-slider");
+
+let activeImageSlide = 0;
+
+productImages.forEach((item, index) =>{
+    item.addEventListener('click',() =>{
+        productImages[activeImageSlide].classList.remove('active');
+        item.classList.add('active');
+        productImageSlide.style.backgroundImage = `url('${item.src}')`;
+        activeImageSlide = index;
+    })
+})
+
+
 let ratingStarInput = [...document.querySelectorAll('.rating-star')];
 
 ratingStarInput.map((star, index) => {
