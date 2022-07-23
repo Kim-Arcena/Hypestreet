471c5fce3afce7a529c798cd94f1d4748022ba37
Kimberly Arcena <kvarcena@up.edu.ph>
2022-07-23 23:52:48 +0800

    redo nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -7,56 +7,56 @@ window.addEventListener('scroll', () => {
     }
 });
 
-const createNavbar = () => {
-    let navbar = document.createElement('navbar');
+// const createNavbar = () => {
+//     let navbar = document.createElement('navbar');
 
-    navbar.innerHTML += `
-    <a href="#home">home</a>
-    <a href="#products">products</a>
-    <a href="#about">about</a>
+//     navbar.innerHTML += `
+//     <a href="#home">home</a>
+//     <a href="#products">products</a>
+//     <a href="#about">about</a>
     
-    <div class="user-interactions">
-        <div class="cart">
-            <a href="#" class="fas fa-shopping-cart"></a>
-            <span class="cart-item-count">00</span>
-        </div>
-        <div class="user">
-            <a class="fas fa-user"></a>
-            <div class="user-icon-popup active">
-                <p>Login to your account</p>
-                <a>Login</a>
-            </div>
-        </div>
-    </div>
-    `
+//     <div class="user-interactions">
+//         <div class="cart">
+//             <a href="#" class="fas fa-shopping-cart"></a>
+//             <span class="cart-item-count">00</span>
+//         </div>
+//         <div class="user">
+//             <a class="fas fa-user"></a>
+//             <div class="user-icon-popup active">
+//                 <p>Login to your account</p>
+//                 <a>Login</a>
+//             </div>
+//         </div>
+//     </div>
+//     `
     
-}
+// }
 
-createNavbar();
+// createNavbar();
 
-//user icon pop uo
-let userIcon = document.querySelector('.user');
-let userPopupIcon = document.querySelector('.user-icon-popup');
+// //user icon pop uo
+// let userIcon = document.querySelector('.user');
+// let userPopupIcon = document.querySelector('.user-icon-popup');
 
-userIcon.addEventListener('click', ()=> userPopupIcon.classList.toggle('active'));
+// userIcon.addEventListener('click', ()=> userPopupIcon.classList.toggle('active'));
 
-let text = userPopupIcon.querySelector('p');
-let actionBtn = userPopupIcon.querySelector('a');
-let user = JSON.parse(sessionStorage.user || null);
+// let text = userPopupIcon.querySelector('p');
+// let actionBtn = userPopupIcon.querySelector('a');
+// let user = JSON.parse(sessionStorage.user || null);
 
-if(user != null){
-    text.innerHTML = `Welcome ${user.name}`;
-    actionBtn.innerHTML = 'Logout';
-    actionBtn.addEventListener('click', () => logout());
-}
-else{ 
-    actionBtn.innerHTML = 'Login to your account';
-    actionBtn.innerHTML = 'login';
-    actionBtn.addEventListener('click', () => location.href = '/login');
+// if(user != null){
+//     text.innerHTML = `Welcome ${user.name}`;
+//     actionBtn.innerHTML = 'Logout';
+//     actionBtn.addEventListener('click', () => logout());
+// }
+// else{ 
+//     actionBtn.innerHTML = 'Login to your account';
+//     actionBtn.innerHTML = 'login';
+//     actionBtn.addEventListener('click', () => location.href = '/login');
     
-}
+// }
 
-const logout = () => {
-    sessionStorage.clear();
-    location.reload();
-}
+// const logout = () => {
+//     sessionStorage.clear();
+//     location.reload();
+// }
