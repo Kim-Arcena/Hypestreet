ecbd0c29bfee80b963ccb1a8177861d290b4fb0e
Kimberly Arcena <kvarcena@up.edu.ph>
2022-07-28 22:17:36 +0800

    fill star

--- public/js/product.js
+++ public/js/product.js
@@ -32,10 +32,10 @@ ratingStarInput.map((star, index) => {
     star.addEventListener('click', () => {
         for (let i = 0; i < 5; i++){
             if(i <= index){
-                ratingStarInput[i].src = 'img/product-des/fill-star.png';
+                ratingStarInput[i].src = '../img/product-des/fill-star.png';
             }
             else{
-                ratingStarInput[i].src = 'img/product-des/no-fill-star.png';
+                ratingStarInput[i].src = '../img/product-des/no-fill-star.png';
             }
             console.log("starrr");
         }
