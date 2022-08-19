00a79830f0a3b2a7bf7f0d5ec938885abc88f37e
Kimberly Arcena <kvarcena@up.edu.ph>
2022-08-19 23:45:05 +0800

    add dropdown to nav

--- public/js/nav.js
+++ public/js/nav.js
@@ -14,31 +14,52 @@ const createNavbar = () => {
 
     navbar.innerHTML += `
         <ul>
-            <li class="logo"><a href="/">HYPSTRT.ILO</a></li>
-            <li class="nav-btn"><span class="fas fa-bars"></span></li>
-            <div class="items">
-                <li><a href="/">home</a></li>
-                <li><a href="/product-list/all">products</a></li>
-                <li><a href="/about">about</a></li>
-                <li class="cart" onclick="location.href = '/cart'">
-                    <a href="#" class="fas fa-shopping-cart"></a>
-                    <span class="cart-item-count">00</span>
-                </li>
-                <li class="user">
-                    <a class="fas fa-user"></a>
-                    <div class="user-icon-popup">
-                        <p>Login to your account</p>
-                        <a class="log-btn">Login</a>
-                    </div>
-                </li>   
-            </div>
-            <li class="search-icon">
-                <input class="search" type="search" placeholder="Search">
-                <label class="icon">
-                <span class="fas fa-search"></span>
-                </label>
+        <li class="logo"><a href="/">HYPSTRT.ILO</a></li>
+        <li class="nav-btn"><span class="fas fa-bars"></span></li>
+        <div class="items">
+            <li><a href="/">home</a></li>
+            <li><a href="/product-list/all">products<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                <ul>
+                    <li><a href="/product-list/yeezy">Yeezy</a></li>
+                    <li><a href="/product-list/nike">Nike<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                        <ul class="types">
+                            <li><a href="/product-list/dunks">Sb dunk</a></li>
+                            <li><a href="/product-list/air max">Air Max</a></li>
+                            <li><a href="/product-list/air force">Air Force</a></li>
+                        </ul>
+                    </li>   
+                    <li><a href="/product-list/air jordan">Air Jordan<i class="fa fa-caret-down" aria-hidden="true"></i></a>
+                        <ul class="types">
+                            <li><a href="/product-list/aj1 high">Air Jordan 1 High</a></li>
+                            <li><a href="/product-list/aj1 mid">Air Jordan Mid</a></li>
+                            <li><a href="/product-list/aj1 low">Air Jordan Low</a></li>
+                            <li><a href="/product-list/aj3">Air Jordan 3</a></li>
+                            <li><a href="/product-list/aj4">Air Jordan 4</a></li>
+
+                        </ul>
+                    </li>
+                </ul>
             </li>
-        </ul>
+            <li><a href="/about">about</a></li>
+            <li class="cart" onclick="location.href = '/cart'">
+                <a href="#" class="fas fa-shopping-cart"></a>
+                <span class="cart-item-count">00</span>
+            </li>
+            <li class="user">
+                <a class="fas fa-user"></a>
+                <div class="user-icon-popup">
+                    <p>Login to your account</p>
+                    <a class="log-btn">Login</a>
+                </div>
+            </li>   
+        </div>
+        <li class="search-icon">
+            <input class="search" type="search" placeholder="Search">
+            <label class="icon">
+            <span class="fas fa-search"></span>
+            </label>
+        </li>
+    </ul>
     `   
 }
 
