9ffb080fafa2fc890b58109f694f12ddcb0ed8ec
Kimberly Arcena <kvarcena@up.edu.ph>
2022-08-20 23:51:59 +0800

    parallax

--- public/js/home.js
+++ public/js/home.js
@@ -18,6 +18,41 @@ function prev(){
     slides[index].classList.add('active');
 }
 
+var image = document.getElementsByClassName('ione');
+      new simpleParallax(image, {
+            scale: 1.8
+      });
+
+      var image = document.getElementsByClassName('itwo');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'left'
+      });
+
+      var image = document.getElementsByClassName('ithree');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'right'
+      });
+
+      var image = document.getElementsByClassName('ifour');
+      new simpleParallax(image, {
+            scale: 1.6,
+      });
+
+      var image = document.getElementsByClassName('ifive');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'right'
+      });
+
+      var image = document.getElementsByClassName('isix');
+      new simpleParallax(image, {
+            scale: 1.6,
+            orientation: 'left'
+      });
+
+
 const getProducts = (tag) => {
   return fetch('/get-products', {
       method: 'post',
