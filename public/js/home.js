d0b5900e745faaebc4ef3220d3b9f83dff1940a8
Kimberly Arcena <kvarcena@up.edu.ph>
2022-08-23 23:28:59 +0800

    responsive checkout

--- public/js/home.js
+++ public/js/home.js
@@ -123,36 +123,3 @@ const addProductToCart = (product) => {
         return 'added to cart';
     }
 }
-var image = document.getElementsByClassName('ione');
-new simpleParallax(image, {
-    scale: 1.8
-});
-
-var image = document.getElementsByClassName('itwo');
-new simpleParallax(image, {
-    scale: 1.6,
-    orientation: 'left'
-});
-
-var image = document.getElementsByClassName('ithree');
-new simpleParallax(image, {
-    scale: 1.6,
-    orientation: 'right'
-});
-
-var image = document.getElementsByClassName('ifour');
-new simpleParallax(image, {
-    scale: 1.6,
-});
-
-var image = document.getElementsByClassName('ifive');
-new simpleParallax(image, {
-    scale: 1.6,
-    orientation: 'right'
-});
-
-      var image = document.getElementsByClassName('isix');
-      new simpleParallax(image, {
-            scale: 1.6,
-            orientation: 'left'
-      });
